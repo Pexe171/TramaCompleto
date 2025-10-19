f57656467c07c9d78b2208a381bfe86e3fe30fbc
Pexe <davidhenriqusms18@gmail.com>
2025-10-19 18:22:24 -0400

    Organizar rotas de admin para exibir login

--- app/admin/login/page.jsx
+++ app/admin/login/page.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
-import { apiClient, storeAuthToken } from '../../../lib/apiClient';
+import { apiClient, storeAuthToken } from '../../../../lib/apiClient';
 
 const initialState = { email: '', password: '' };
 
